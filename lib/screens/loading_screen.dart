74b7ce7527f8d3b50617deb648975e16f24d6b43
devhowey <dev.howey@gmail.com>
2022-02-11 14:50:14 -0600

    set up geolocation and permissions

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,6 +7,15 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  // querying to get user geolocation; works for iOS and Android devices
+  void getLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        // more accuracy translates to more battery being used on users device
+        desiredAccuracy: LocationAccuracy.low);
+    // TODO: Delete print() before production build
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +23,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: RaisedButton(
           onPressed: () {
             //Get the current location
+            getLocation();
           },
           child: const Text('Get Location'),
         ),
